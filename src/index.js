b8819384a8355128dd9624a58cdf71fc7a33c087
amarti333 <alejandram.j333@gmail.com>
2022-01-31 09:21:40 -0800

    reordered the layout of the code in JS
    also removed unnecessary code that I did not need within my code.

--- src/index.js
+++ src/index.js
@@ -48,21 +48,24 @@ formating();
 
 function displayTemp(response) {
   let temperatrureElement = document.querySelector("#temperature");
+  temperatrureElement.innerHTML = Math.round(celsiusTemp);
+
   let city = document.querySelector("#city");
-  let windSpeedElement = document.querySelector("#wind");
-  let humidityElement = document.querySelector("#humid");
-  let iconElement = document.querySelector("#icon");
-
-  celsiusTemperature = response.data.main.temp;
-
-  temperatrureElement.innerHTML = Math.round(response.data.main.temp);
   city.innerHTML = response.data.name;
-  humidityElement.innerHTML = response.data.main.humidity;
+
+  let windSpeedElement = document.querySelector("#wind");
   windSpeedElement.innerHTML = Math.round(response.data.wind.speed);
+
+  let humidityElement = document.querySelector("#humid");
+  humidityElement.innerHTML = response.data.main.humidity;
+
+  let iconElement = document.querySelector("#icon");
   iconElement.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+
+  celsiusTemp = response.data.main.temp;
 }
 
 function search(city) {
@@ -84,35 +87,33 @@ function displayFahrenheitTemp(event) {
   let temperatrureElement = document.querySelector("#temperature");
   temperatrureElement.innerHTML = fahrenheiteTemp;
 }
-
-let celsiusTemp = null;
-
-search("Salt Lake City");
+celsiusTemp = null;
 
 let form = document.querySelector("#city-form");
 form.addEventListener("submit", mainSearch);
+search("Salt Lake City");
 
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemp);
-
-let celsiuslink = document.querySelector("#celsius-link");
-celsiuslink.addEvent;
 
 //Bonus task week 5:
 function currentWeather() {
   function showLocation(position) {
     console.log(position.coords.latitude);
     console.log(position.coords.longitude);
-    let latitude = position.coords.latitude;
-    let longitude = position.coords.longitude;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=fcff6baa99a6aaa2ecb4f015dd030bde&units=metric`;
+    let latitudeElement = position.coords.latitude;
+    let longitudeElement = position.coords.longitude;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitudeElement}&lon=${longitudeElement}&appid=fcff6baa99a6aaa2ecb4f015dd030bde&units=metric`;
     axios.get(apiUrl).then(showTemperature);
   }
+
   function showTemperature(response) {
+    console.log(response.data.main.temp);
     let temp = Math.round(response.data.main.temp);
     let h1 = document.querySelector("h1");
     h1.innerHTML = `Current temperature outside is ${temp}°C`;
   }
+
   navigator.geolocation.getCurrentPosition(showLocation);
 }
 
